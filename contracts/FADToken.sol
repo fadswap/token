60761997fb7bbae26ae8378042086edec20f01c9
Tony NGUEREZA <nguerezatony@gmail.com>
2022-05-02 07:06:03 +0100

    Add support for token burn

--- contracts/FADToken.sol
+++ contracts/FADToken.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract FADToken is Context, ERC20, Ownable {
+
+contract FADToken is Context, ERC20, Ownable, ERC20Burnable {
     constructor() ERC20("FAD Swap", "$FAD") {
         _mint(msg.sender, 100000000 * 10 ** decimals());
     }
